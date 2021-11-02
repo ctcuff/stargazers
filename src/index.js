ef5afd55c5a73ce2e68283b67b4f338981b54ae2
Joseph Terribile <kartingman25@gmail.com>
2021-11-02 16:16:12 -0400

    Simple game/render loop

--- src/index.js
+++ src/index.js
@@ -8,7 +8,53 @@ const programInfo = twgl.createProgramInfo(gl, [vs, fs], error =>
   console.log(error)
 );
 
-gl.clearColor(0.5, 0.2, 0.7, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
+// object containing what keys are down for a animation frame
+const keyDown = {};
+
+document.querySelector('body').addEventListener('keydown', (e) => {
+  keyDown[e.code] = true;
+});
+
+document.querySelector('body').addEventListener('keyup', (e) => {
+  keyDown[e.code] = false;
+});
+
+// init gl stuff here, like back face culling and the depth test
 gl.enable(gl.DEPTH_TEST);
-gl.useProgram(programInfo.program);
+gl.clearColor(0.5, 0.2, 0.7, 1.0);
+
+// start the render loop by requesting an animation frame for the frame function
+let rafHandle = requestAnimationFrame(frame);
+
+// track when the last frame rendered
+let lastFrameMilis = 0;
+
+// create looper function
+function frame(curentMilis) {
+  // calculate the change in time in seconds since the last frame
+  let deltaTime = (curentMilis - lastFrameMilis) / 1000;
+
+  // update things here
+  update(deltaTime);
+
+  // clear the previous frame
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // do the render
+  render(deltaTime);
+
+  // request the next frame
+  rafHandle = requestAnimationFrame(frame);
+
+  // update the last frame milis
+  lastFrameMilis = curentMilis;
+}
+
+function update(deltaTime) {
+  console.log(keyDown);
+}
+
+function render(deltaTime) {
+  gl.useProgram(programInfo.program);
+  // twgl.drawBufferInfo or what ever the call is i dont remember
+}
