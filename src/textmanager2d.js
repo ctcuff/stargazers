e07e18d76b541716b61e5007dc674e6a959c2478
Marco Peric <mperic2001@gmail.com>
2021-11-20 14:30:43 -0500

    Removed comment from top of class.

--- src/textmanager2d.js
+++ src/textmanager2d.js
@@ -1,7 +1,3 @@
-// This is a very rudimentary setup which is designed to simply display some text as per the '#overlay' section of the CSS file.
-// When complete, it could include some functionality to accomodate animations, resizing, and other effects.
-// A TextManager3D class may come in the future, which would ideally be implemented with our WebGL context.
-
 class TextManager2D
 {
     constructor()
