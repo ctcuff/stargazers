469f768d421039870f77c7a5a3599f5ff8c90445
Joseph Terribile <kartingman25@gmail.com>
2021-11-22 16:29:09 -0500

    Enhance frame buffer to be modular and have multisample and multitarget

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,7 @@
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl2');
 
-export { gl };
+// the number of samples for a multisample frame buffer
+const multiSampleSamples = 4;
+
+export { gl, multiSampleSamples };
