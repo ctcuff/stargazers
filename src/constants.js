91781413fe85b8058e29338b4ace3d1b54e8a49a
Joseph Terribile <kartingman25@gmail.com>
2021-11-24 01:01:14 -0500

    Ensure browser antialiasing is off (we do it manually in my town)

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector('canvas');
-const gl = canvas.getContext('webgl2');
+const gl = canvas.getContext('webgl2', {
+  antialias: false
+});
 
 // the number of samples for a multisample frame buffer
 const multiSampleSamples = 4;
