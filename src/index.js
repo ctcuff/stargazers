1b4663a90041a1368d33d899fb8a1ca12cc1ec64
unknown <AlexanderALongo@knights.ucf.edu>
2021-11-30 13:29:46 -0500

    Ton of changes for everything needed to load a texture bind it to a model and render it

--- src/index.js
+++ src/index.js
@@ -25,13 +25,20 @@ const main = async () => {
   // track when the last frame rendered
   let lastFrameMilis = 0;
 
+  const textures = twgl.createTextures(gl, {
+    asteroid1: { src: require('./models/asteroid1_extras/textures/rock_Base_Color.png'), flipY: true, wrap: gl.REPEAT, mag: gl.LINEAR, min: gl.MIPMAP},
+    ufo: { src: require('./models/ufo_extras/ufo_diffuse.png'), flipY: true, wrap: gl.REPEAT, mag: gl.LINEAR, min: gl.MIPMAP},
+    rayman: { src: require('./models/rayman.png'), flipY: true, wrap: gl.REPEAT, mag: gl.LINEAR, min: gl.MIPMAP}
+  })
+
+  // Other models are currently commented out as they dont have a texture
   const modelRefs = [
-    { model: require('./models/ufo.obj'), name: 'ufo' },
-    { model: require('./models/starwars.obj'), name: 'starwars' },
-    { model: require('./models/asteroid0.obj'), name: 'asteroid0' },
-    { model: require('./models/asteroid1.obj'), name: 'asteroid1' },
-    { model: require('./models/raymanModel.obj'), name: 'rayman' },
-    { model: require('./models/cow.obj'), name: 'cow' }
+    { model: require('./models/ufo.obj'), name: 'ufo', texture: textures.ufo},
+    //{ model: require('./models/starwars.obj'), name: 'starwars' },
+    { model: require('./models/asteroid0.obj'), name: 'asteroid0', texture: textures.asteroid1 },
+    { model: require('./models/asteroid1.obj'), name: 'asteroid1', texture: textures.asteroid1 },
+    { model: require('./models/raymanModel.obj'), name: 'rayman', texture: textures.rayman },
+    //{ model: require('./models/cow.obj'), name: 'cow' }
   ];
 
   await manager.addModels(modelRefs);
--- src/shaders/shader.vert
+++ src/shaders/shader.vert
@@ -2,14 +2,21 @@
 
 in vec3 position;
 in vec3 normal;
+in vec2 uv;
 
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 
 out vec3 fragNormal;
+out vec3 fragPosition;
+out vec2 fragUV;
 
 void main () {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    fragNormal = normalize(normal);
+    fragUV = uv;
+    vec4 newPosition = modelMatrix*vec4(position,1);
+    fragPosition = newPosition.xyz;
+    gl_Position = projectionMatrix*viewMatrix*newPosition;
+    mat4 normalMatrix = transpose(inverse(modelMatrix));
+    fragNormal = normalize((normalMatrix*vec4(normal,0)).xyz);
 }
