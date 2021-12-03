2146f6509337ccdf27f3582215704cdc166ba90d
Joseph Terribile <kartingman25@gmail.com>
2021-12-03 18:04:27 -0500

    Make arrow keys modify camera rotation

--- src/gameobjects/ufo.js
+++ src/gameobjects/ufo.js
@@ -8,6 +8,7 @@ import ShieldProjectile from './shield-projectile';
 import gameEventEmitter from '../utils/game-event-emitter';
 import uiManager from '../textmanager2d';
 import { GameEvents } from '../constants';
+import { deg2rad } from '../utils/math';
 
 class UFO extends GameObject {
   constructor() {
@@ -35,28 +36,31 @@ class UFO extends GameObject {
 
     Input.addKeyPressListener('space', this.fireProjectile.bind(this));
     Input.addClickListener(this.fireProjectile.bind(this));
+
+    this.pitch = 20;
+    this.yaw = 0;
   }
 
   update(deltaTime) {
     // Key mapping:
 
     // Right movement
-    if (Input.keysDown.ArrowRight || Input.keysDown.d || Input.keysDown.D || Input.keysDown.e) {
+    if (Input.keysDown.d || Input.keysDown.D || Input.keysDown.e) {
       this.position.add(new Vector3(this.strafeSpeedX, 0, 0));
     }
 
     // Left movement
-    if (Input.keysDown.ArrowLeft || Input.keysDown.a || Input.keysDown.q) {
+    if (Input.keysDown.a || Input.keysDown.q) {
       this.position.add(new Vector3(-this.strafeSpeedX, 0, 0));
     }
 
     // Up movement
-    if (Input.keysDown.ArrowUp || Input.keysDown.w || Input.keysDown.e || Input.keysDown.q) {
+    if (Input.keysDown.w || Input.keysDown.e || Input.keysDown.q) {
       this.position.add(new Vector3(0, this.strafeSpeedY, 0));
     }
 
     // Down movement
-    if (Input.keysDown.ArrowDown || Input.keysDown.s) {
+    if (Input.keysDown.s) {
       this.position.add(new Vector3(0, -this.strafeSpeedY, 0));
     }
 
@@ -82,8 +86,31 @@ class UFO extends GameObject {
       this.strafeSpeedY += 0.25;
     }
 
+    if (Input.keysDown.f) {
+      this.physics.velocity = new Vector3();
+    }
+
+    if (Input.keysDown.ArrowRight) {
+      this.yaw += 120 * deltaTime;
+    }
+
+    if (Input.keysDown.ArrowLeft) {
+      this.yaw -= 120 * deltaTime;
+    }
+
+    if (Input.keysDown.ArrowUp) {
+      this.pitch -= 120 * deltaTime;
+    }
+
+    if (Input.keysDown.ArrowDown) {
+      this.pitch += 120 * deltaTime;
+    }
+
     // Fix the camera so it's positioned behind the ship each frame
-    const offset = new Vector3(0, this.extents.dia * 0.35, this.extents.dia * 0.9);
+    // const offset = new Vector3(0, this.extents.dia * 0.35, this.extents.dia * 0.9);
+    let horLen = this.extents.dia * Math.cos(deg2rad(this.pitch));
+    let verLen = this.extents.dia * Math.sin(deg2rad(this.pitch));
+    const offset = new Vector3(horLen * Math.sin(deg2rad(this.yaw)), verLen, horLen * Math.cos(deg2rad(this.yaw)));
     manager.camera.setPosition(this.position.clone().add(offset));
     manager.camera.lookAt(this.position);
 
